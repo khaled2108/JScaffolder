056a4445fb14b74f17893183653620c421871bc0
khaled <halleb.khaled@gmail.com>
2020-10-23 15:41:36 +0200

    uml2 java exemple

---
+++ UML2-Exemple/generated-source/java/TestJScaffolderEntity.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * 2020, All rights reserved.
+ *******************************************************************************/
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of TestJScaffolderEntity.
+ * 
+ * @author kha
+ */
+public class TestJScaffolderEntity {
+	/**
+	 * Description of the property id.
+	 */
+	public Integer id = Integer.valueOf(0);
+	
+	/**
+	 * Description of the property name.
+	 */
+	public String name = "";
+	
+	/**
+	 * Description of the property description.
+	 */
+	public String description = "";
+	
+	// Start of user code (user defined attributes for TestJScaffolderEntity)
+	
+	// End of user code
+	
+	/**
+	 * The constructor.
+	 */
+	public TestJScaffolderEntity() {
+		// Start of user code constructor for TestJScaffolderEntity)
+		super();
+		// End of user code
+	}
+	
+	// Start of user code (user defined methods for TestJScaffolderEntity)
+
+	// End of user code
+	/**
+	 * Returns id.
+	 * @return id 
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+	
+	/**
+	 * Sets a value to attribute id. 
+	 * @param newId 
+	 */
+	public void setId(Integer newId) {
+	    this.id = newId;
+	}
+
+	/**
+	 * Returns name.
+	 * @return name 
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * Sets a value to attribute name. 
+	 * @param newName 
+	 */
+	public void setName(String newName) {
+	    this.name = newName;
+	}
+
+	/**
+	 * Returns description.
+	 * @return description 
+	 */
+	public String getDescription() {
+		return this.description;
+	}
+	
+	/**
+	 * Sets a value to attribute description. 
+	 * @param newDescription 
+	 */
+	public void setDescription(String newDescription) {
+	    this.description = newDescription;
+	}
+
+}
